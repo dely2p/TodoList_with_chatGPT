2771fdf4c9915c9cb4693d6c14e7a197f3f6a674
dely <lhyal2p@gmail.com>
2023-04-23 15:46:06 +0900

    프로젝트 생성

---
+++ TodoList_with_chatGPT/TodoList_with_chatGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList_with_chatGPT
+//
+//  Created by elly on 2023/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
